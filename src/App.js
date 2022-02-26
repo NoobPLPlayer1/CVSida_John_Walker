5fca8fefb78cdeae7011c3c4e55ca108a174e49f
NoobPLPlayer1 <johnwalker0x62@gmail.com>
2022-02-26 12:50:05 +0100

    labb2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Lang from './components/lang';
+
+function App() {
+  return (
+    <div>
+      <Lang /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Lang from './components/lang';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Lang /> 
+  </React.StrictMode>,
+  document.getElementById('grid')
+);
